e5eaedd725246b2da202a607a41772bdb011501d
Yağız Bora <yaizbora@gmail.com>
2025-10-19 00:05:40 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace SystemInformation_Basic_Taskmanager_in_console_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "System Information & Basic Taskmanager in console";
+
+
+
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("===SYSTEM INFORMATION===");
+                Console.WriteLine("\n===BASIC TASKMANAGER===");
+
+
+                using (var proc = Process.GetCurrentProcess())
+                {
+                    long memBytes = proc.WorkingSet64;
+                    Console.WriteLine($"App Memory Usage: {memBytes / 1024 / 1024} MB");
+                }
+
+                try
+                {
+                    var pc = new PerformanceCounter("Memory", "Available MBytes");
+                    Console.WriteLine($"Available Memory: {pc.NextValue()} MB");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Available RAM: (PerformanceCounter not supported on this platform)");
+                }
+
+
+                Console.WriteLine("\n===SYSTEM DETAILS===");
+
+                Console.WriteLine($"Machine Name: {Environment.MachineName}");
+                Console.WriteLine($"Operating System: {Environment.OSVersion}");
+                Console.WriteLine($"Processor Count: {Environment.ProcessorCount}");
+                Console.WriteLine($"System Directory: {Environment.SystemDirectory}");
+                Console.WriteLine($"User Domain Name: {Environment.UserDomainName}");
+                Console.WriteLine($"64-Bit Operating System: {Environment.Is64BitOperatingSystem}");
+                Console.WriteLine($"Cpu Procces Count:  {Environment.ProcessorCount}");
+                Console.WriteLine($"User Name: {Environment.UserName}");
+                Console.WriteLine($"CLR Version: {Environment.Version}");
+
+                //foreach (var process in processes)
+                //{
+                //    try
+                //    {
+                //        Console.WriteLine($"{process.Id,-10} {process.ProcessName,-30} {process.WorkingSet64 / (1024 * 1024),-15:N2}");
+                //    }
+                //    catch
+                //    {
+                //        // Some system processes might not allow access to their properties
+                //    }
+                //}
+                Console.WriteLine("\nPress Ctrl+C to exit.");
+                Thread.Sleep(5000);
+            }
+            
+        }
+    }
+}
